ef8054d15296202fc7e99818e40cae2fee900e2d
Gachinski <easywin.ru@gmail.com>
2020-11-01 00:05:20 +0300

    Initial commit.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    // setTimeout(function(){
+    //     location.replace('./computer.html');
+    // }, (Math.random() * (12 - 4) + 4)*1000);
+    let i = 1;
+    1
+    let text = 'Loading';
+    setInterval(function () {
+        if (i > 5) {
+            i = 1;
+        } else {
+            i++;
+        }
+        document.title = text + '.'.repeat(i);
+    }, 300);
+};
